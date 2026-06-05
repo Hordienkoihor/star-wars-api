bc1969b86b65ebd6f9627d5711adfb4be02bed95
Hordienkoihor <hordienkoihor05@gmail.com>
2026-06-05 15:05:03 +0300

    swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe({transform: true}));
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle("Star Wars API")
+        .setDescription("Star Wars information on any entity API")
+        .setVersion("1.0")
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, documentFactory);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
--- src/people/model/people.model.ts
+++ src/people/model/people.model.ts
@@ -1,63 +1,93 @@
 import {IsArray, IsISO8601, IsNotEmpty, IsNumber, IsNumberString, IsString} from "class-validator";
 import {Type} from "class-transformer";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class CreatePeopleDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
     @IsNumberString()
     // @Type(() => Number)
     height: string;
 
+    @ApiProperty()
     @IsNumberString()
     // @Type(() => Number)
     mass: string;
 
+    @ApiProperty()
     @IsString()
     hair_color: string;
 
+    @ApiProperty()
     @IsString()
     skin_color: string;
 
+    @ApiProperty()
     @IsString()
     eye_color: string;
 
+    @ApiProperty()
     @IsString()
     birth_year: string;
 
+    @ApiProperty()
     @IsString()
     gender: string;
 
+    @ApiProperty()
     @IsString()
     homeworld: string;
 
+    @ApiProperty({
+        description: 'in future will be changed to id array',
+        type: [String],
+    })
     @IsArray()
     @IsString({each: true})
     films: string[];
 
+    @ApiProperty({
+        description: 'in future will be changed to id array',
+        type: [String],
+    })
     @IsArray()
     @IsString({each: true})
     species: string[];
 
+    @ApiProperty({
+        description: 'in future will be changed to id array',
+        type: [String],
+    })
     @IsArray()
     @IsString({each: true})
     vehicles: string[];
 
+    @ApiProperty({
+        description: 'in future will be changed to id array',
+        type: [String],
+    })
     @IsArray()
     @IsString({each: true})
     starships: string[];
 
+    @ApiProperty()
     @IsISO8601()
     created: string;
 
+    @ApiProperty()
     @IsISO8601()
     edited: string;
 
+    @ApiProperty()
     @IsString()
     url: string
 
+    @ApiProperty()
     @IsArray()
     @IsString({each: true})
     imgs: string[];
