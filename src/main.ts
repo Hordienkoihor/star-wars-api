8b5766c2a055cf9aa98fe6a0c72d3bdf13eddc5f
Hordienkoihor <hordienkoihor05@gmail.com>
2026-06-15 21:19:38 +0300

    feat: response interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ResponseInterceptor} from "./interceptor/response-interceptor";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalInterceptors(new ResponseInterceptor());
 
     app.useGlobalPipes(new ValidationPipe({transform: true}));
     const swaggerConfig = new DocumentBuilder()
