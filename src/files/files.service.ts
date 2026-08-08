bb7192fdccad1b51fd4e4a2a31d8d75c100bf7e2
Hordienkoihor <hordienkoihor05@gmail.com>
2026-08-08 15:36:17 +0300

    fix: fix user and credentials on bucket

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -12,18 +12,25 @@ import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 @Injectable()
 export class FilesService {
     private readonly s3Client: S3Client;
+    private readonly bucket: string;
 
     constructor(private readonly configService: ConfigService) {
         this.s3Client = new S3Client({
-            region: this.configService.getOrThrow('AWS_S3_REGION')
+            region: this.configService.getOrThrow('AWS_S3_REGION'),
+            credentials: {
+                accessKeyId: configService.getOrThrow('AWS_S3_ACCESS_KEY'),
+                secretAccessKey: configService.getOrThrow('AWS_S3_SECRET_ACCESS_KEY')
+            }
         });
+
+        this.bucket = configService.getOrThrow('AWS_S3_BUCKET_NAME');
     }
 
     async upload(filename: string, file: Buffer): Promise<string> {
        try {
            await this.s3Client.send(
                new PutObjectCommand({
-                   Bucket: 'starwars-api-bucket-265315779869-eu-north-1-an',
+                   Bucket: this.bucket,
                    Key: filename,
                    Body: file
                })
